b974a3ba76d7e0b5d628e53475ac64ba2abc2382
glosier <glosier@customer.io>
2021-02-03 17:16:51 -0800

    let's try adding an id to the script

--- bundles/redoc.lib.js
+++ bundles/redoc.lib.js
@@ -7686,7 +7686,7 @@ var ErrorBoundary_ErrorBoundary = /** @class */ (function (_super) {
                 external_react_["createElement"]("br", null),
                 external_react_["createElement"]("small", null,
                     " Commit: ",
-                    "4772f888"));
+                    "a810adb5"));
         }
         return external_react_["Children"].only(this.props.children);
     };
